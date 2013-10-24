5d42627e6c78675b50003210a8fc3142b329f925
Rafid K. Abdullah <rafidka@gmail.com>
2013-10-25 00:00:02 +0100

    Adding Basmala, chapter title, and morphology (empty) table to morphology page.

--- website/morphology/static/morphology/js/index.js
+++ website/morphology/static/morphology/js/index.js
@@ -1,42 +1,58 @@
-function loadVerses() {
+$(function() {
   'use strict';
-  var chapterIndex = $('#chapterList').val();
-  var verseCount = QuranInfo.verseCounts[chapterIndex];
-  var verseList = $('#verseList');
-  var verseSelector;
-  var i;
 
-  verseList.html('');
-  for (i = 0; i < verseCount; i += 1) {
-    verseSelector = 'Verse {0}:{1}'.format(
-        i + 1,
-        verseCount);
-    $('<option/>').attr('value', i).text(verseSelector).appendTo(verseList);
-  }
+  var loadVerses = function() {
+    var chapterIndex = $('#chapterList').val();
+    var verseCount = QuranInfo.verseCounts[chapterIndex];
+    var verseList = $('#verseList');
+    var verseSelector;
+    var i;
 
-}
+    verseList.html('');
+    for (i = 0; i < verseCount; i += 1) {
+      verseSelector = 'Verse {0}:{1}'.format(
+          i + 1,
+          verseCount);
+      $('<option/>').attr('value', i).text(verseSelector).appendTo(verseList);
+    }
+  };
 
-function loadChapters() {
-  'use strict';
-  var i, chapterSelector, chapterList;
-  chapterList = $('#chapterList');
-  chapterList.html('');
-  for (i = 0; i < QuranInfo.chapterCount; i += 1) {
-    chapterSelector = 'Chapter ({0}) {1} ({2})'.format(
-        i + 1,
-        QuranInfo.chapterNamesTransliterated[i],
-        QuranInfo.chapterNamesInEnglish[i]);
-    $('<option/>').attr('value', i).text(chapterSelector).appendTo(chapterList);
-  }
+  var loadChapters = function() {
+    var i, chapterSelector, chapterList;
+    chapterList = $('#chapterList');
+    chapterList.html('');
+    for (i = 0; i < QuranInfo.chapterCount; i += 1) {
+      chapterSelector = 'Chapter ({0}) {1} ({2})'.format(
+          i + 1,
+          QuranInfo.chapterNamesTransliterated[i],
+          QuranInfo.chapterNamesInEnglish[i]);
+      $('<option/>').attr('value', i + 1)
+          .text(chapterSelector)
+          .appendTo(chapterList);
+    }
 
-  chapterList.change(function() {
-    loadVerses();
-  });
-}
+    chapterList.change(function() {
+      loadVerses();
+    });
+  };
 
-$(document).ready(function() {
-  'use strict';
+  var loadMorphology = function() {
+    var chapterList = $('#chapterList');
+    var chapterIndex = chapterList.val();
+    var chapterName = chapterList.find('option:selected').text();
+    $('#chapterName').text(chapterName);
+    if (chapterIndex === '9') {
+      // We don't show the Basmala if the selected sūrat is l-tawbah.
+      $('#basmala').hide();
+    }
+    else {
+      $('#basmala').show();
+    }
+    $('#morphologyTable').show();
+  };
+
+  $('#loadMorphology').click(loadMorphology);
+
   loadChapters();
   loadVerses();
 });
-
