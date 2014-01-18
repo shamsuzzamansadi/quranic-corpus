478407513aefb041c3eb1068090090666033ba21
Rafid K. Abdullah <rafidka@gmail.com>
2014-01-18 11:35:10 +0000

    Adding licence information.

--- website/static/js/string.js
+++ website/static/js/string.js
@@ -1,5 +1,5 @@
 /**
- * Contains extensions to String object.
+ * Contains extensions to JavaScript string object.
  * @author rafidka@gmail.com (Rafid K. Abdullah)
  */
 
@@ -7,11 +7,16 @@ if (!String.prototype.format) {
   /**
    * Formats a string by replacing placeholders of the form {...} with the
    * corresponding given indexed or named arguments.
+   *
+   * The arguments are a set of indexed and named arguments to replace the
+   * placeholders in the string.
+   *
    * This is taken from this from this Stack Overflow answer:
    * http://stackoverflow.com/a/4673436/196697
-   * The arguments A set of indexed and named arguments to replace
-   * the placeholders in the string.
-   * @return {String} Returns the formatted string.
+   * and is thus bound by StackOverflow licence, which is licenced under
+   * CC-BY-SA 3.0 at the time of writing this comment, Jan-18 2014.
+   *
+   * @return {String} The formatted string.
    */
   String.prototype.format = function() {
     'use strict';
