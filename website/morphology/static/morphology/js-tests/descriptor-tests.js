99a794de72a9c4fd68af50f00f4417a82348fcec
Rafid K. Abdullah <rafidka@gmail.com>
2014-01-23 21:49:17 +0000

    Adding tests for parsing the features and generating the description of the segments up to the fourth verse of Surat Al-Fatiha.

--- website/morphology/static/morphology/js-tests/descriptor-tests.js
+++ website/morphology/static/morphology/js-tests/descriptor-tests.js
@@ -19,6 +19,8 @@
  * the Quranic Arabic Corpus. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*global equal*/
+
 test('Generate b/bi/ Description', function() {
   'use strict';
   var segment = new Morphology.Segment({
@@ -120,4 +122,140 @@ test('Generate b/r~aHiymi/ Description', function() {
   var descriptor = new Morphology.Descriptor();
   var desc = descriptor.generateSegmentDescription(segment);
   equal(desc, 'genitive masculine singular adjective');
+});
+
+test('Generate b/Hamodu/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 1,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'Hamodu',
+    'segment_id': 1002001002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:Hamod|ROOT:Hmd|M|NOM'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'nominative masculine noun');
+});
+
+test('Generate b/li/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'P',
+    'chapter_no': 1,
+    'form': 'li',
+    'segment_id': 1002002001,
+    'segment_no': 1,
+    'verse_no': 2,
+    'features': 'PREFIX|l:P+'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'prefixed preposition');
+});
+
+test('Generate b/l~ahi/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'PN',
+    'chapter_no': 1,
+    'form': 'l~ahi',
+    'segment_id': 1002002002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:PN|LEM:{ll~ah|ROOT:Alh|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive proper noun');
+});
+
+test('Generate b/rab~i/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 3,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'rab~i',
+    'segment_id': 1002003001,
+    'segment_no': 1,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:rab~|ROOT:rbb|M|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive masculine noun');
+});
+
+test('Generate b/Ea`lamiyna/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 4,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'Ea`lamiyna',
+    'segment_id': 1002004002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:Ea`lamiyn|ROOT:Elm|MP|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive masculine plural noun');
+});
+
+test('Generate b/ma`liki/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 1,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'ma`liki',
+    'segment_id': 1004001001,
+    'segment_no': 1,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|DER:ACT PCPL|LEM:ma`lik|ROOT:mlk|M|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive masculine active participle');
+});
+
+test('Generate b/yawomi/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'yawomi',
+    'segment_id': 1004002001,
+    'segment_no': 1,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|LEM:yawom|ROOT:ywm|M|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive masculine noun');
+});
+
+test('Generate b/d~iyni/ Description', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 3,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'd~iyni',
+    'segment_id': 1004003002,
+    'segment_no': 2,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|LEM:diyn|ROOT:dyn|M|GEN'
+  });
+  var descriptor = new Morphology.Descriptor();
+  var desc = descriptor.generateSegmentDescription(segment);
+  equal(desc, 'genitive masculine noun');
 });
--- website/morphology/static/morphology/js-tests/segment-tests.js
+++ website/morphology/static/morphology/js-tests/segment-tests.js
@@ -19,6 +19,8 @@
  * the Quranic Arabic Corpus. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*global equal*/
+
 test('Extract b/bi/ Features', function() {
   'use strict';
   var segment = new Morphology.Segment({
@@ -135,5 +137,170 @@ test('Extract b/r~aHiymi/ Features', function() {
   equal(segment.root, 'rHm');
   equal(segment.gender.value, Morphology.Gender.Masculine);
   equal(segment.number.value, Morphology.Number.Singular);
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/Hamodu/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 1,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'Hamodu',
+    'segment_id': 1002001002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:Hamod|ROOT:Hmd|M|NOM'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.lemma, 'Hamod');
+  equal(segment.root, 'Hmd');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
+  equal(segment.case.value, Morphology.SegmentCase.Nominative);
+});
+
+test('Extract b/li/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'P',
+    'chapter_no': 1,
+    'form': 'li',
+    'segment_id': 1002002001,
+    'segment_no': 1,
+    'verse_no': 2,
+    'features': 'PREFIX|l:P+'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Prefix);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Preposition);
+});
+
+test('Extract b/l~ahi/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'PN',
+    'chapter_no': 1,
+    'form': 'l~ahi',
+    'segment_id': 1002002002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:PN|LEM:{ll~ah|ROOT:Alh|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.ProperNoun);
+  equal(segment.lemma, '{ll~ah');
+  equal(segment.root, 'Alh');
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/rab~i/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 3,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'rab~i',
+    'segment_id': 1002003001,
+    'segment_no': 1,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:rab~|ROOT:rbb|M|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.lemma, 'rab~');
+  equal(segment.root, 'rbb');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/Ea`lamiyna/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 4,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'Ea`lamiyna',
+    'segment_id': 1002004002,
+    'segment_no': 2,
+    'verse_no': 2,
+    'features': 'STEM|POS:N|LEM:Ea`lamiyn|ROOT:Elm|MP|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.lemma, 'Ea`lamiyn');
+  equal(segment.root, 'Elm');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
+  equal(segment.number.value, Morphology.Number.Plural);
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/ma`liki/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 1,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'ma`liki',
+    'segment_id': 1004001001,
+    'segment_no': 1,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|DER:ACT PCPL|LEM:ma`lik|ROOT:mlk|M|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.derivation.value, Morphology.Derivation.ActiveParticiple);
+  equal(segment.lemma, 'ma`lik');
+  equal(segment.root, 'mlk');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/yawomi/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 2,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'yawomi',
+    'segment_id': 1004002001,
+    'segment_no': 1,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|LEM:yawom|ROOT:ywm|M|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.lemma, 'yawom');
+  equal(segment.root, 'ywm');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
+  equal(segment.case.value, Morphology.SegmentCase.Genitive);
+});
+
+test('Extract b/d~iyni/ Features', function() {
+  'use strict';
+  var segment = new Morphology.Segment({
+    'token_no': 3,
+    'tag': 'N',
+    'chapter_no': 1,
+    'form': 'd~iyni',
+    'segment_id': 1004003002,
+    'segment_no': 2,
+    'verse_no': 4,
+    'features': 'STEM|POS:N|LEM:diyn|ROOT:dyn|M|GEN'
+  });
+
+  equal(segment.type.value, Morphology.SegmentType.Stem);
+  equal(segment.partOfSpeech.value, Morphology.PartOfSpeeh.Noun);
+  equal(segment.lemma, 'diyn');
+  equal(segment.root, 'dyn');
+  equal(segment.gender.value, Morphology.Gender.Masculine);
   equal(segment.case.value, Morphology.SegmentCase.Genitive);
 });
